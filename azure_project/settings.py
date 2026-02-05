3192dda75dbbbf608b830cb971da283d737d921b
MangkornC <87025222+MangkornC@users.noreply.github.com>
2026-02-05 15:57:54 +0700

    mongodb

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -100,16 +100,16 @@ DATABASES = {
         },
     },
 }
-
+# mongodb+srv://mangkornadmin:<password>@mangkorn-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000
 
 import urllib.parse
 
 # MongoDB Configuration
-mongo_username = urllib.parse.quote_plus('bearlab')
-mongo_password = urllib.parse.quote_plus('P@ss1234')
+mongo_username = urllib.parse.quote_plus('mangkornadmin')
+mongo_password = urllib.parse.quote_plus('abcdefg_555')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@bearlab-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
-MONGO_DB_NAME = 'django_store_reviews'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@mangkorn-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_DB_NAME = 'mangkorn-mongodb'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
